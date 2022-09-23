d05de4e1a82a6d06e1dd3fbc845d895c39437c9c
diegoperezvar <81699245+diegoperezvar@users.noreply.github.com>
2022-09-23 17:21:17 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,64 @@
+let members = [
+"Austria","Belgium","Bulgaria","Croatia","Republic of Cyprus","Czech Republic", "Denmark","Estonia","Finland","France","Germany","Greece","Hungary","Ireland","Italy","Latvia","Lithuania","Luxembourg","Malta","Netherlands","Poland","Portugal","Romania","Slovakia","Slovenia","Spain","Sweden"
+];
+let lastIndex = members.length 
+// ahora la información de cada país
+var randomItem = members[Math.floor(Math.random()*members.length)];   
+console.log(lastIndex+" is the total number of EU members")
+console.log(randomItem+" is the selected country")
+// y el nombre oficial
+if (randomItem === members[25]){
+  console.log("official name: kingdom of Spain")
+  console.log("Viva españa")}
+  if (randomItem === members[23]){
+  console.log("official name:Slovak republic")}
+  if (randomItem === members[24]){
+  console.log("official name:Slovenian republic")}
+  if (randomItem === members[22]){
+  console.log("official name:Romania")}
+  if (randomItem === members[21]){
+  console.log("official name:portuguese republic")}
+  if (randomItem === members[20]){
+  console.log("official name:Polish Republic")}
+  if (randomItem === members[26]){
+  console.log("official name:Kingdom of Sweden")}
+  if (randomItem === members[19]){
+  console.log("official name:kingdom of the Netherlands")}
+if (randomItem === members[18]){
+  console.log("official name:Republic of Malta")}
+if (randomItem === members[17]){
+ console.log("official name:the Grand Duchy of Luxembourg")}
+ if (randomItem === members[16]){
+ console.log("official name:lituanian republic")}
+ if (randomItem === members[15]){
+ console.log("official name:latvian republic")}
+ if (randomItem === members[14]){
+ console.log("official name:italian Republic")}
+  if (randomItem === members[13]){
+ console.log("official name:Irish Republic")}
+  if (randomItem === members[12]){
+ console.log("official name:Hungarian Republic")}
+  if (randomItem === members[11]){
+ console.log("official name:Greek Helenic Republic")}
+  if (randomItem === members[10]){
+ console.log("official name:German Federal Republic")}
+  if (randomItem === members[9]){
+ console.log("official name:French Republic")}
+  if (randomItem === members[8]){
+ console.log("official name:Finish Republic")}
+  if (randomItem === members[7]){
+ console.log("official name:Estonian Republic")}
+  if (randomItem === members[6]){
+ console.log("official name:Kingdom of Denmark")}
+  if (randomItem === members[5]){
+ console.log("official name:czechia")}
+  if (randomItem === members[4]){
+ console.log("official name:Republic of Cyprus")}
+  if (randomItem === members[3]){
+ console.log("official name:Croatian Republic")}
+  if (randomItem === members[2]){
+ console.log("official name:Bulgarian Republic")}
+  if (randomItem === members[1]){
+ console.log("official name:Kingdom of Belgium")}
+  if (randomItem === members[0]){
+ console.log("official name:Austrian Republic")}
